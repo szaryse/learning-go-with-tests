3c0463906e6739211c74969c2e1a0b2463b469d7
Szaryse <szaryseb@gmail.com>
2025-05-31 19:37:57 +0200

    Add maps tests

---
+++ maps/dictionary.go
@@ -0,0 +1,73 @@
+package maps
+
+import (
+	"errors"
+)
+
+type Dictionary map[string]string
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
+)
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch {
+	case errors.Is(err, ErrNotFound):
+		d[word] = definition
+	case err == nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch {
+	case errors.Is(err, ErrNotFound):
+		return ErrWordDoesNotExist
+	case err == nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch {
+	case errors.Is(err, ErrNotFound):
+		return ErrWordDoesNotExist
+	case err == nil:
+		delete(d, word)
+	default:
+		return err
+	}
+
+	return nil
+}
